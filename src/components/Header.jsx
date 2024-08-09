338f6770660af3fd60922672ee0a586778d0cb22
HeinLemmer <lemmerhein@gmail.com>
2024-08-09 12:57:36 +0200

    mail update

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -33,7 +33,7 @@ const Header = () => {
 
   useEffect(() => {
     // Obfuscate email
-    const user = "NetgrowthSolutions";
+    const user = "NetgrowthITSolutions";
     const domain = "outlook.com";
     setEmail(`${user}@${domain}`);
   }, []);
